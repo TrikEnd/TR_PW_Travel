a7b47f3c437a33c7ed9db733f5465be1cb24a701
TrikEnd <patrikgampang@gmail.com>
2025-11-18 11:57:12 +0700

    navbar

---
+++ component/navbar.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style/navbar.css">
+</head>
+<body>
+    <<nav class="navbar">
+    <div class="nav-container">
+        <ul class="nav-menu">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Package</a></li>
+            <li><a href="#">Blog</a></li>
+        </ul>
+    </div>
+</nav>
+
+</body>
+</html>
