9be251734aa1e0fcfd9f4f64d6e588a0c522bb1c
Dzeck <dzakiachmadamrullah@gmail.com>
2025-11-20 06:39:17 +0700

    Add files via upload

---
+++ TRpw/index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="nav/navbar.css">
+    <link rel="stylesheet" href="index.css">
+</head>
+<body>
+
+    <?php include 'nav/navbar.php'; ?>
+
+    <div class="popup">
+    
+        <div class="popup-content">
+            <h1>Login</h1>
+            <input type="email" placeholder="example@Gmailcom">
+            <input type="password" placeholder="*****">
+        
+        <label class="remember">
+            <input type="checkbox"> Remember me
+
+             <p> Dont have account?<b><a class="link" href="">Register</a></b></p>
+        </label>
+
+                      
+            <button>Login</button>
+        </div>
+        </div>
+   
+
+
+    <!-- <div class="popup">
+        <div class="popup-content">
+            <h1>Register</h1>
+            <input type="text" placeholder= "Username">
+            <input type="email" placeholder="example@Gmailcom">
+            <input type="password" placeholder="Password">
+            <input type="password" placeholder="Password confirmation">
+            <div class="link">
+
+        <label class="remember">
+            <input type="checkbox"> Remember me
+
+             <p >Have account?<b><a class="link" href="">Login</a></b></p>
+        </label>
+            </div>
+        
+            <button>Login</button>
+        </div>
+    </div> -->
+
+</body>
+</html>
+
---
+++ TRpw/nav/navbar.php
@@ -0,0 +1,24 @@
+<nav class="navbar">
+    <div class="nav-container">
+
+        <!-- KIRI -->
+        <div class="nav-left">
+            <img src="logo.png" class="logo" alt="Logo">
+        </div>
+
+        <!-- TENGAH -->
+        <ul class="nav-menu">
+            <li><a href="index.php">Home</a></li>
+            <li><a href="tiket.php">Tiket</a></li>
+            <li><a href="#">Package</a></li>
+            <li><a href="#">Blog</a></li>
+        </ul>
+
+        <!-- KANAN -->
+        <div class="nav-right">
+            <a href="#" class="btn login">Login</a>
+            <a href="#" class="btn register">Register</a>
+        </div>
+
+    </div>
+</nav>
